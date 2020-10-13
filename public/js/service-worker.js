d4c88bd761d52178f8021d547cbfa00ee24492fd
layc41 <64671431+layc41@users.noreply.github.com>
2020-10-12 20:40:33 -0500

    added more files to cache

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,53 +1,68 @@
-const APP_PREFIX = 'BudgetTracker-';     
+const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
-
-// list of files to cache
+//files to cache
 const FILES_TO_CACHE = [
-    './index.html',
-    './js/index.js',
-    './js/idb.js',
-    './css/styles.css'
-  ];
+  "./index.html",
+  "./css/styles.css",
+  "./js/idb.js",
+  "./js/index.js",
+  "./manifest.json",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
+];
 
-// install service worker
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('installing cache: ' + CACHE_NAME);
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-  });
-  
-// activate event
-  self.addEventListener('activate', function(e) {
-    e.waitUntil(
-      caches.keys().then(function(keyList) {
-        let cacheKeepList = keyList.filter(function(key) {
-          return key.indexOf(APP_PREFIX);
-        });
-        cacheKeepList.push(CACHE_NAME);
-  
-        return Promise.all(
-          keyList.map(function(key, i) {
-            if (cacheKeepList.indexOf(key) === -1) {
-              console.log('deleting cache:' + keyList[i]);
-              return caches.delete(keyList[i]);
-            }
-          })
-        );
-      })
-    );
-  });
-  
-  // fetch from cache
-  self.addEventListener('fetch', function(e) {
-    console.log('fetch request: ' + e.request.url);
-    e.respondWith(
-      caches.match(e.request).then(function(request) {
-        return request || fetch(e.request)
-      })
-    );
-  });
+// install service workers
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(function(cache) {
+      console.log('installing cache: ' + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// activate workers
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      let cacheKeepList = keyList.filter(function(key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      cacheKeepList.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function(key, i) {
+          if (cacheKeepList.indexOf(key) === -1) {
+            console.log('deleting cache:' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
+
+// fetch info from cache
+self.addEventListener('fetch', function(event) {
+  console.log('fetch request: ' + event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(request) {
+      if (request) {
+        console.log('responding with cache:' + event.request.url);
+        return request;
+      } else {
+        console.log('file is not cached, fetching:' + event.request.url);
+        return fetch(event.request);
+      }
+    })
+  );
+});
+
